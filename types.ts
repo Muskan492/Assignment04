dbf5f2acdad44ffec22bdb1e2d2bac776842eb79
Muskan492 <irshadalilashari5419@gmail.com>
2024-10-04 19:20:44 -0700

    Add files via upload

---
+++ types.ts
@@ -0,0 +1,12 @@
+export interface CardProps {
+
+    name:string,
+    age:number,
+    day:string,
+    rollNumber: number,
+    time:string, 
+    cardBtnText:string
+}
+export interface ButtonProps {
+    btnText:string
+}
